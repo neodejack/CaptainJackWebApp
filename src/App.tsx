ff2dcbc8ac770275045386e1c02ead796dcaa54e
zili <neodejack@gmail.com>
2022-12-03 17:37:14 +0800

    set up solid js

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import type { Component } from 'solid-js';
+import Button from "./components/Button";
+
+
+const App: Component = () => {
+    return (
+        <div>
+            App is here
+            <Button />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import {Component} from "solid-js";
+
+const Button: Component = () => {
+    return (
+        <div>
+            <h2>
+                a button
+            </h2>
+        </div>
+    )
+}
+
+export default Button;
