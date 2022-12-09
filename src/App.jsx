cda7d761a049fc57ab30ef2c5296b57e52bcb4b9
zili <neodejack@gmail.com>
2022-12-09 16:35:12 +0800

    integrate member checking feature with pocketbase

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,16 @@ import MemberPage from "./pages/memberPage.jsx";
 import { createEffect, createSignal } from "solid-js";
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase("http://127.0.0.1:8090");
 const tele = window.Telegram.WebApp;
 tele.ready();
 const webAppData = tele.initDataUnsafe;
 
 const [teleHandle, setTeleHandle] = createSignal("");
-const [member, setMember] = createSignal("");
-setTeleHandle(webAppData.user.username);
-createEffect(async () => {
-  const record = await pb
-    .collection("member_22_23")
-    .getFirstListItem(`telegram_handle=${teleHandle()} && paid=true`);
-  setMember(await record.json());
-});
+try {
+  setTeleHandle(webAppData.user.username);
+} catch (TypeError) {
+  setTeleHandle("");
+}
 
 function App() {
   return (
@@ -27,10 +23,8 @@ function App() {
         🎾 Captain <span class="badge text-bg-success">Jack</span>
       </h1>
       {/*<Nav />*/}
-      <h3>Init Data (unsafe): </h3>
-      <pre id="initDataUnsafe">{member()}</pre>
       <Routes>
-        <Route path="/" element={<MemberPage member={teleHandle()} />} />
+        <Route path="/" element={<MemberPage teleHandle={teleHandle()} />} />
       </Routes>
     </div>
   );
--- src/pages/memberPage.jsx
+++ src/pages/memberPage.jsx
@@ -1,12 +1,32 @@
 import CollapseButton from "../components/collapseButton";
-import { Show } from "solid-js";
+import { createEffect, createSignal, Show } from "solid-js";
 import CourtAllocationButton from "../components/courtAllocationButton";
+import PocketBase from "pocketbase";
 
-const MemberPage = ({ member }) => {
+const pb = new PocketBase("http://127.0.0.1:8090");
+const authData = await pb.admins.authWithPassword(
+  "neodejack@gmail.com",
+  "password123"
+);
+
+const MemberPage = ({ teleHandle }) => {
+  const [member, setMember] = createSignal("");
+  createEffect(async () => {
+    try {
+      const filter = `telegram_handle="${teleHandle + "2"}" && paid=true`;
+      const record = await pb
+        .collection("member_22_23")
+        .getFirstListItem(filter);
+      console.log(record);
+      setMember(await record["name"]);
+    } catch (ClientResponseError) {
+      console.log("error caught");
+    }
+  });
 
   return (
     <Show
-      when={member !== ""}
+      when={member() !== ""}
       fallback={
         <div class="container text-center">
           <h2>You are not a member</h2>
@@ -16,7 +36,7 @@ const MemberPage = ({ member }) => {
       keyed
     >
       <div class="container text-center">
-        <h3>Hey {member}, how can i help</h3>
+        <h3>Hey {member()}, how can i help</h3>
         <CourtAllocationButton />
       </div>
     </Show>
