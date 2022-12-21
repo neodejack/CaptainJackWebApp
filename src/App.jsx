c0485600e939c94f3ef784ec45377abb1ec569d4
zili <neodejack@gmail.com>
2022-12-21 15:09:44 +0800

    deployed changes to flyio

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const [teleHandle, setTeleHandle] = createSignal("");
 try {
   setTeleHandle(webAppData.user.username);
 } catch (TypeError) {
-  setTeleHandle("neodejack");
+  setTeleHandle("");
 }
 
 function App() {
