0294a88d643122ab080cc2d6642c087a67ee3bb2
zili <neodejack@gmail.com>
2022-12-30 22:22:36 +0800

    change to tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        AOblue: "#0091D2",
+      },
+    },
+  },
+  plugins: [],
+};
